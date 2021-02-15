9d9673ac432ca1d2680818a2f7948fe2ac4afec2
Henry Knight <hhknight@ucdavis.edu>
2021-02-15 13:04:45 +0900

    coach responds. no design

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,2 +1,13 @@
 class QuestionsController < ApplicationController
+  def ask
+  end
+  def answer
+    if params[:answer] == 'I am going to work'
+      @response = 'Great!'
+    elsif params[:answer][-1] == '?'
+      @response = 'Silly question, get dressed and go to work!'
+    else
+      @response = "I don't care, get dress and go to work!"
+    end
+  end
 end
